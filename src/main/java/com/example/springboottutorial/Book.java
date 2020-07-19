b8a1c2ab5d7fd1b1f0baebb195f9f0e51e4e9333
takuyam98634217 <takuyama045@gmail.com>
2020-07-19 21:58:49 +0900

    add Lombok and Controller sample

---
+++ src/main/java/com/example/springboottutorial/Book.java
@@ -0,0 +1,12 @@
+package com.example.springboottutorial;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Book {
+    Integer id;
+    String title;
+    String author;
+}
